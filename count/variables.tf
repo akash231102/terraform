2dadda13d4630245f2ccdfd21ed2290935c52a7c
akash <bugataakash@gmail.com>
2025-10-19 13:45:31 +0530

    terraform

---
+++ count/variables.tf
@@ -0,0 +1,11 @@
+variable "instances" {
+    default = ["mysql","mongodb","redis"]
+}
+
+variable "zone-id" {
+    default = "Z0654896JT7QXFW2AAXW"
+}
+
+variable "domain_name" {
+    default = "bugata.shop"
+}
